8c042ce0ea337e81e0e46d47d4a1559e64da0563
Eegans <emiegans@gmail.com>
2021-09-10 20:59:29 -0400

    Routing, adding styles, first page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '../app/shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../material.module';
+import { LoaderComponent } from './loader/loader.component';
+
+
+@NgModule({
+  imports: [MaterialModule, CommonModule],
+  declarations: [LoaderComponent],
+  providers: [],
+  exports: [LoaderComponent]
+})
+export class SharedModule {}
